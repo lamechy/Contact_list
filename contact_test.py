72e950f5a233cea4ea96cc46f9b03e66d2018f37
lamechy <lamechongondi88@gmail.com>
2020-06-26 10:15:28 +0300

    Created the file for the TDD tests for this application

--- contact_test.py
+++ contact_test.py
@@ -1,47 +1,83 @@
 import unittest 
+import pyperclip 
 from contact import Contact 
 class TestContact(unittest.TestCase):
-    def setUp(self):
-        '''
-        Set up method to run before each test cases.
-        '''
-        self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com") # create contact object
+   
+   def setUp(self):
+       self.new_contact = Contact("Nyakundi","Lamech","0790406870","Ongondi88@gmail.com")# Create contact object
 
+   def tearDown(self):
+       Contact.contact_list = []
 
-    def test_init(self):
-        '''
-        test_init test case to test if the object is initialized properly
-        '''
+   def test_init(self):
+       self.assertEqual(self.new_contact.first_name,"Nyakundi")
+       self.assertEqual(self.new_contact.last_name,"Lamech")
+       self.assertEqual(self.new_contact.phone_number,"0790406870")
+       self.assertEqual(self.new_contact.email,"Ongondi88@gmail.com")
 
-        self.assertEqual(self.new_contact.first_name,"James")
-        self.assertEqual(self.new_contact.last_name,"Muriuki")
-        self.assertEqual(self.new_contact.phone_number,"0712345678")
-        self.assertEqual(self.new_contact.email,"james@ms.com")
+   def setUp(self):
+       self.new_contact = Contact("Nyakundi","Lamech","0790406870","Ongondi88@gmail.com")
 
-
-
-
-    def setUp(self):
-        '''
-        Set up method to run before each test cases.
-        '''
-        self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com") # create contact object
-    def test_save_contact(self):
-        '''
-        test_save_contact test case to test if the contact object is saved into
-         the contact list
-        '''
-        self.new_contact.save_contact() # saving the new contact
+   def test_save_contact(self):
+        self.new_contact.save_contact()# saving the contact 
         self.assertEqual(len(Contact.contact_list),1)
-    def test_save_multiple_contact(self):
-            '''
-            test_save_multiple_contact to check if we can save multiple contact
-            objects to our contact_list
-            '''
-            self.new_contact.save_contact()
-            test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
-            test_contact.save_contact()
-            self.assertEqual(len(Contact.contact_list),2)
+   def setUp(self):
+       self.new_contact = Contact("Nyakundi","Lamech","0790406870","Ongondi88@gmail.com")
+  
+   
+   def test_save_multiple_contact(self):
 
-if __name__ ==  '__main__':
+       self.new_contact.save_contact()
+       test_contact = Contact("Test","user","0790406870","test@user.com")
+       test_contact.save_contact()
+       self.assertEqual(len(Contact.contact_list),2)
+
+   def setUp(self):
+       self.new_contact = Contact("Nyakundi","Lamech","0790406870","Ongondi88@gmail.com")
+
+   def test_delete_contact(self):
+       self.new_contact.save_contact()
+       test_contact = Contact("Test","user","0790406870","test@user.com")
+       test_contact.save_contact()
+
+       self.new_contact.delete_contact()
+       self.assertEqual(len(Contact.contact_list),1)
+
+   def setUp(self):
+       self.new_contact = Contact("Nyakundi","Lamech","0790406870","Ongondi88@gmail.com")
+   def test_find_contact_by_number(self):
+       self.new_contact.save_contact()
+       test_contact = Contact("Test","user","0791406870","test@user.com")
+       test_contact.save_contact()
+
+       found_contact = Contact.find_by_number("0791406870")
+
+       self.assertEqual(found_contact.email,test_contact.email)
+
+   def setUp(self):
+       self.new_contact = Contact("Nyakundi","Lamech","0790406870","Ongondi88@gmail.com")
+   def test_contact_exists(self):
+       self.new_contact.save_contact()
+       test_contact =  Contact("Test","user","0791406870","Ongondi88@gmail.com")
+       test_contact.save_contact()
+
+       contact_exists = Contact.contact_exist("0791406870")
+       self.assertTrue(contact_exists)
+
+   def setUp(self):
+       self.new_contact = Contact("Nyakundi","Lamech","0790406870","Ongondi88@gmail.com")
+   def test_diplay_all_contacts(self):
+       self.assertEqual(Contact.display_contacts(),Contact.contact_list)
+
+   def setUp(self):
+       self.new_contact = Contact("Nyakundi","Lamech","0790406870","Ongondi88@gmail.com")
+
+   def test_copy_email(self):
+       self.new_contact.save_contact()
+       Contact.copy_email("0790406870")
+
+       self.assertEqual(self.new_contact.email,pyperclip.paste())
+
+if __name__ == '__main__':
     unittest.main()
+ 
