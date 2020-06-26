fb0bda38943ded60d040781cf3edf3ccf4cfa988
lamechy <lamechongondi88@gmail.com>
2020-06-26 10:13:23 +0300

    created the main contact application file

--- contact.py
+++ contact.py
@@ -1,7 +1,9 @@
+import pyperclip 
 class Contact:
     """
     Class that generates new instances of contacts.
     """
+    contact_list = [] # Empty contact list
     def __init__(self,first_name,last_name,number,email):
 
       # docstring removed for simplicity
@@ -10,8 +12,32 @@ class Contact:
         self.last_name = last_name
         self.phone_number = number
         self.email = email
-    contact_list = [] # Empty contact list
+   
     def save_contact(self):
         Contact.contact_list.append(self)
+
+    def delete_contact(self):
+        Contact.contact_list.remove(self)
+    @classmethod
+    def find_by_number(cls,number):
+
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
+    @classmethod
+    def contact_exist(cls,number):
+
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                    return True
+
+        return False               
+    @classmethod
+    def display_contacts(cls):
+        return cls.contact_list
+    @classmethod
+    def copy_email(cls,number):
+        contact_found = Contact.find_by_number(number)
+        pyperclip.copy(contact_found.email)    
 
     
